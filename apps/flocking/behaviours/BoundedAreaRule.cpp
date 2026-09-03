c1a2630bb8f8776773c19f29b42f20c99d2c890c
ColinSk <colin.skaarup@mymail.champlain.edu>
2026-09-03 12:20:52 -0400

    fixed boundedarea

--- apps/flocking/behaviours/BoundedAreaRule.cpp
+++ apps/flocking/behaviours/BoundedAreaRule.cpp
@@ -10,31 +10,29 @@ glm::vec2 BoundedAreaRule::computeForce(const std::vector<BoidView>& neighborhoo
 
   // begin solution
 
-  float tempForce = 0.0f;
+  //float tempForce = 0.0f;
 
-  const float width = displaySize.x;
-  const float height = displaySize.y;
+  const float width = displaySize.x > 0.0f ? displaySize.x : 1280.0f;
+  const float height = displaySize.y > 0.0f ? displaySize.y : 800.0f;
 
   const glm::vec2 worldBorder(width, height);
   const glm::vec2 zeroVec(0.0f);
 
   if (boid.position.x > worldBorder.x - (float)desiredDistance) {
-    tempForce = (float)desiredDistance / abs(boid.position.x - worldBorder.x);
+    force.x = ((float)desiredDistance / abs(boid.position.x - worldBorder.x)) * -1;
   }
 
   else if (boid.position.x < zeroVec.x + (float)desiredDistance) {
-    tempForce = (float)desiredDistance / abs(boid.position.x - zeroVec.x);
+    force.x = (float)desiredDistance / abs(boid.position.x - zeroVec.x);
   }
 
-  else if (boid.position.y > worldBorder.y - (float)desiredDistance) {
-    tempForce = (float)desiredDistance / abs(boid.position.y - worldBorder.y);
+  if (boid.position.y > worldBorder.y - (float)desiredDistance) {
+    force.y = ((float)desiredDistance / abs(boid.position.y - worldBorder.y)) * -1;
   }
 
   else if (boid.position.y < zeroVec.y + (float)desiredDistance) {
-    tempForce = (float)desiredDistance / abs(boid.position.y - zeroVec.y);
+    force.y = ((float)desiredDistance / abs(boid.position.y - zeroVec.y));
   }
-
-  force += /*dir **/ tempForce;
   // end solution
 
   return force;
